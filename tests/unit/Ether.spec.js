b0d6d2409f7cd73c8bac9f1ffe52ddd30e080393
eskemojoe007 <David.Folkner@gmail.com>
2018-06-13 20:00:08 -0400

    Added beforeEach

--- tests/unit/Ether.spec.js
+++ tests/unit/Ether.spec.js
@@ -1,7 +1,11 @@
-import {shallowMount} from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 import Ether from "@/components/Ether.vue";
 
 describe("Ether.vue", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(Ether);
+  });
   it("renders props.msg when passed", () => {
     const msg = "new message";
     const wrapper = shallowMount(Ether, {
@@ -11,23 +15,19 @@ describe("Ether.vue", () => {
   });
   it("looks for default message when nothing passed", () => {
     const msg = "default ether";
-    const wrapper = shallowMount(Ether);
     expect(wrapper.text()).toMatch(msg);
   });
   it("gets the default price and checks it", () => {
     const price = 200;
-    const wrapper = shallowMount(Ether);
     expect(wrapper.vm.price).toBe(price);
   });
   it("Tests the function increasePrice", () => {
-    const wrapper = shallowMount(Ether);
     const price_up = 100;
     const price = wrapper.vm.price;
     wrapper.vm.increasePrice(price_up);
     expect(wrapper.vm.price).toBe(price + price_up);
   });
   it("Tests the button push", () => {
-    const wrapper = shallowMount(Ether);
     const price = wrapper.vm.price;
     const button = wrapper.find("button");
     const price_up = 100;
